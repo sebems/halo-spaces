98a71fe38f0915e61f1ca505b8aa8fa5658220b4
Sean Ebenmelu <jindusean@gmail.com>
2025-01-29 16:59:19 -0500

    added goals for main

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from helper import *
+
+# TODO: setup page navigation
+
+# TODO: make a dataframe that holds classroom assets
+
+# TODO: make the classroom assets displayable via model components or cards
+
+# TODO: add filter mechanic
+
+# TODO: add search bar
+
+# TODO: link classroom assets to images
+
+# TODO: link classrooms to their respective assets
