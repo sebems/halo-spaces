1f14d5a6bc05ac898a9adeaed896a4f223105536
Sean Ebenmelu <sce22@calvin.edu>
2025-02-05 10:42:01 -0500

    redid the page config and added more documentation

--- assets_helper.py
+++ assets_helper.py
@@ -10,6 +10,7 @@ base_URL = "https://halo.calvin.edu/api"
 auth_URL = "https://halo.calvin.edu/auth/token?tenant=calvinuni"
 asset_URL = base_URL + "/asset"
 CLIENT_ID, CLIENT_SECRET = st.secrets["CLIENT_ID"], st.secrets["CLIENT_SECRET"]
+
 ASSET_GROUP_ID = 103  # asset group id for classrooms
 DEBUG = False
 
@@ -37,6 +38,7 @@ building_codes = {
 }
 
 
+@st.cache_data  # caches the data to avoid long renders and reruns
 def getToken():
     """
     Gets API Token for session. This has read and edit access to assets in Halo ITSM
@@ -48,6 +50,7 @@ def getToken():
     return response.json()["access_token"] if response.ok else ""
 
 
+@st.cache_data
 def getClassDetails(token, id):
     try:
         if len(token) > 0:  # check if the token is empty
@@ -72,6 +75,7 @@ def getClassDetails(token, id):
         print(err, "Token is empty")
 
 
+@st.cache_data
 def getClassrooms(token):
     """
     Gets the list of Classrooms from Halo in Dictionary variable
@@ -107,6 +111,7 @@ def getClassrooms(token):
         print(err, "Token is empty")
 
 
+@st.cache_data
 def getClassRoomsCondensed(token):
     """
     Gets the list of Classrooms Assets from Halo in a Codensed Dictionary variable
@@ -160,7 +165,6 @@ def getClassRoomsCondensed(token):
                 # room assets
                 # room capacity
                 # crestron availability
-
 
                 modi_classes[halo_building_name].append(
                     [
--- main.py
+++ main.py
@@ -3,24 +3,36 @@ import pandas as pd
 from assets_helper import getToken, getClassRoomsCondensed
 from attachments_helper import getAttachmentsByHaloID, getAttachmentImage
 
+#######  PAGE CONFIG AND LOGO  #######
+st.set_page_config(
+    page_title="Halo Spaces' Assets",
+    page_icon="./images/calvin.png",
+    layout="wide",
+    menu_items={"Get Help": "https://helpdesk.calvin.edu/portal/"},
+)
+
+## Main Logo
+st.logo(image="./images/calvin_banner_black.png")
+
+## Main Header with Divider
+st.header("Spaces Assets [Draft]", divider="red")
+
+## PAGE COLUMNS
+table_col, details_col = st.columns([1, 3])  # the details_col has more space allotted
+
 TOKEN = getToken()
-HEADERS = ["Room Name", "Room ID", "Room Capacity"]
-
-st.header("Spaces Assets [Draft]")
-
-st.divider()
-
-table_col, details_col = st.columns(2)
-config = {"Room ID": st.column_config.NumberColumn("Room ID", format="%d")}
+COL_HEADERS = ["Room Name", "Room ID", "Room Capacity"]
 
 
-def createClassExpander(room_name: str, room_id: int, room_cap: int, isCrestronAvailable: bool):
+def createClassExpander(
+    room_name: str, room_id: int, room_cap: int, isCrestronAvailable: bool
+):
     """
     Creates an Expander for each space
     """
     a_expander = st.expander(room_name)
 
-    image = ''
+    image = ""
     attachments = getAttachmentsByHaloID(TOKEN, room_id)
 
     if attachments != None:
@@ -40,15 +52,24 @@ def createClassExpander(room_name: str, room_id: int, room_cap: int, isCrestronA
     a_expander.info("Crestron Panel Available", icon=icon)
 
 
+def createImageContainer():
+    pass
+
+
 class_dict = getClassRoomsCondensed(TOKEN)
 
 # SIDEBAR SECTION
 sidebar = st.sidebar
 
 
+## DUMMY FUNCTION FOR SEARCH BTN
+def fiz():
+    pass
+
+
 with sidebar:
     search = st.text_input("Search")
-    submit = st.button("Submit", on_click=print(search))
+    submit = st.button("Submit", on_click=fiz)
     building_options = st.selectbox(
         "Filter Buildings",
         (
@@ -66,7 +87,7 @@ with sidebar:
         ),
     )
 
-capacity_filter = sidebar.slider("Room Capacity", 10, 200, (20, 50))
+capacity_filter = sidebar.slider("Room Capacity", 10, 200, step=10)
 
 with table_col:
     building_choice = f"{building_options}" + " Classrooms"
@@ -74,31 +95,39 @@ with table_col:
     if building_options != None and len(class_dict) > 0:
         building_rooms = class_dict[building_choice]  # gets the building choice
 
+        # DATAFRAME COLUMN CONFIGURATION
+        config = {"Room ID": st.column_config.NumberColumn("Room ID", format="%d")}
+
         # MAIN DATAFRAME OF SPACES ON CAMPUSs
-        class_df = pd.DataFrame(building_rooms, columns=HEADERS)
+        class_df = pd.DataFrame(building_rooms, columns=COL_HEADERS)
 
         if search != "":
             class_df = class_df[class_df["Room Name"] == search]
 
         class_df = class_df[
-            class_df["Room Capacity"] >= min(capacity_filter)
+            class_df["Room Capacity"] >= capacity_filter
         ]  # add room capacity filter
 
+        ## STREAMLIT DATAFRAME DISPLAY
         st.dataframe(
-            class_df, column_config=config, use_container_width=True, hide_index=True
-        )  # display dataframe
+            class_df["Room Name"],  # show only the room names
+            column_config=config,
+            use_container_width=True,
+            hide_index=True,
+        )
     else:
-        st.dataframe(pd.DataFrame())
+        ## DISPLAY AN EMPTY DATAFRAME IF THE API QUERY IS EMPTY
+        st.dataframe(pd.DataFrame(columns=COL_HEADERS))
 
 with details_col:
     for room, room_id, capacity in zip(
-        class_df["Room Name"].tolist(), class_df["Room ID"].tolist(), class_df["Room Capacity"].tolist()
+        class_df["Room Name"].tolist(),
+        class_df["Room ID"].tolist(),
+        class_df["Room Capacity"].tolist(),
     ):
         # apart from the room_name param, the rest are dummy values waiting for change in Halo
         createClassExpander(room, room_id, capacity, True)
 
     # TODO: get classroom details
-
-    # TODO: link classroom assets to images | need to host images in a different repo
 
     # TODO: link classrooms to their respective assets
