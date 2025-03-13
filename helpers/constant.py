7306f77ae47995063547da843c2e4bdbefbd26b8
Sean Ebenmelu <40689266+sebems@users.noreply.github.com>
2025-03-13 11:43:55 -0400

    Update constant.py

--- helpers/constant.py
+++ helpers/constant.py
@@ -55,35 +55,3 @@ BOARD_TYPES = [
     "Whiteboard - Small",
     "None",
 ]
-
-HEADER_COLS = [
-    "CLASSROOM_TYPE",
-    "DISPLAY_TYPE",
-    "CPT",
-    "SEAT_COUNT",
-    "TEAMS_ROOM",
-    "LAST_REFRESH",
-    "LCR_PRICE",
-    "LCR_YEAR",
-    "CAMERA_TYPE",
-    "TIERED_SEATING",
-    "SMART_CLASSROOM",
-    "DESK_TYPE",  # 186
-    "TABLE_TYPE",  # 191
-    "BOARD_TYPE",
-    "PODIUM_TYPE",
-    "SCREEN_TYPE",
-    "RECORD_TYPE",
-    "COMPUTER_LAB",
-    "COMPUTER",
-    "CAMERA_MIC",
-    "MICROPHONE_TYPE",
-    "INPUTS",
-    "DISPLAY_TYPE",
-    "ETHERNET_JACK",
-    "SOUND_SYSTEM",
-    "ADDITIONAL_ROOM_SPECS",
-    "ROOM_ID",
-    "ROOM_NAME",
-    "BUILDING_NAME",
-]
